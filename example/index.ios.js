fdcb2161810621d0e06b6093d03a6449c22b09f7
Konstantin Yakushin <y231966mdk@gmail.com>
2016-07-07 15:52:21 +0600

    Example is prepared.

--- example/index.ios.js
+++ example/index.ios.js
@@ -11,22 +11,40 @@ import {
   Text,
   View
 } from 'react-native';
+import ScrollableTabView from 'react-native-scrollable-tab-view';
+import TabBar from "./tabBar";
+
+const Page = ({label}) => (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>
+        {label}
+      </Text>
+      <Text style={styles.instructions}>
+        To get started, edit index.ios.js
+      </Text>
+      <Text style={styles.instructions}>
+        Press Cmd+R to reload,{'\n'}
+        Cmd+D or shake for dev menu
+      </Text>
+    </View>
+);
 
 class example extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+        <View style={[styles.container, {paddingTop: 20}]}>
+          <ScrollableTabView
+              tabBarUnderlineColor="#53ac49"
+              tabBarActiveTextColor="#53ac49"
+              renderTabBar={() => <TabBar/>}>
+            <Page tabLabel={{label: "Page #1"}} label="Page #1"/>
+            <Page tabLabel={{label: "Page #2 aka Long!", badge: 3}} label="Page #2 aka Long!"/>
+            <Page tabLabel={{label: "Page #3"}} label="Page #3"/>
+            <Page tabLabel={{label: "Page #4 aka Page"}} label="Page #4 aka Page"/>
+            <Page tabLabel={{label: "Page #5"}} label="Page #5"/>
+          </ScrollableTabView>
+
+        </View>
     );
   }
 }
@@ -36,18 +54,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
+    marginBottom: 5
+  }
 });
 
 AppRegistry.registerComponent('example', () => example);
